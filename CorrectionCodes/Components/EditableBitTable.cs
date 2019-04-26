4d612f908442249a0a0f3dc448515926d16c2825
Brat Wiekszy <bratwiekszy@gmail.com>
2019-04-26 13:34:40 +0200

    Dodano komponent do edycji bitów i modele ui

---
+++ CorrectionCodes/Components/EditableBitTable.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CorrectionCodes.Components
+{
+	public sealed class EditableBitTable : ListBox
+	{
+		public ICommand FlipBitCommand { get; private set; } = new FlipBitCommand();
+
+	}
+}
---
+++ CorrectionCodes/Models/BitModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
+namespace CorrectionCodes.Models
+{
+	public sealed class BitModel : INotifyPropertyChanged
+	{
+		private int _numericValue;
+
+		public int NumericValue
+		{
+			get => _numericValue;
+			set
+			{
+				if (value != _numericValue)
+					Modified = !Modified;
+
+				_numericValue = value;
+			}
+		}
+
+		public bool Bool
+		{
+			get => NumericValue == 1; 
+			//set => NumericValue = value ? 1 : 0;
+		}
+
+		public bool Modified { get; set; }
+
+		public BitModel(int numericValue) 
+		{
+			_numericValue = numericValue;
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void FlipBit()
+		{
+			NumericValue = Bool ? 0 : 1;
+		}
+	}
+}
