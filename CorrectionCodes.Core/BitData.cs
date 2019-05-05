ab69079ac3641d14d363abf01c802709a6dfbd5d
Brat Wiekszy <bratwiekszy@gmail.com>
2019-05-05 12:58:09 +0200

    Added some extension and algorithm data exhange model

---
+++ CorrectionCodes.Core/BitData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+
+namespace CorrectionCodes.Core
+{
+	public sealed class BitData
+	{
+		[NotNull] public byte[] CorrectionBits { get; set; }
+		[NotNull] public byte[] TransmittedBits { get; private set; }
+
+		public bool[] DetectedBitErrors { get; set; }
+		public byte[] FixedTransmittedBits { get; set; }
+
+
+		public BitData(byte[] correctionBits, byte[] transmittedBits)
+		{
+			CorrectionBits = correctionBits;
+			TransmittedBits = transmittedBits;
+		}
+	}
+}
---
+++ CorrectionCodes.Core/ICorrectionCode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+
+namespace CorrectionCodes.Core
+{
+	public interface ICorrectionCode
+	{
+		/// <summary>
+		/// Returns redundant correction bits produced from given data bits
+		/// </summary>
+		/// <param name="rawBits"></param>
+		/// <returns></returns>
+		[NotNull] byte[] ComputeCorrectionCode([NotNull] byte[] rawBits);
+
+		/// <summary>
+		/// Sets DetectedBitErrors and FixedTransmittedBits at indexes coresponding to transmitted bit indexes.
+		/// </summary>
+		/// <param name="transmittedData"></param>
+		/// <returns></returns>
+		[NotNull] BitData DetectBitErrors([NotNull] BitData transmittedData);
+	}
+}
