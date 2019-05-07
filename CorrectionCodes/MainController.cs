00afa4a9669281692aeb34dd7b809767c09d98c0
Brat Wiekszy <bratwiekszy@gmail.com>
2019-05-07 11:11:22 +0200

    Added controler for window & couple of controls

---
+++ CorrectionCodes/MainController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CorrectionCodes.Core;
+
+namespace CorrectionCodes
+{
+	public sealed class MainController : INotifyPropertyChanged
+	{
+		private Random _random = new Random();
+		private byte[] _originalBits;
+		
+
+		public string TextData { get; set; }
+		public int    TextLength => TextData.Length;
+		public byte[] TransmittedBits { get; private set; }
+		public bool[] ModifiedBits { get; private set; }
+		public BitData DataModel { get; private set; }
+
+
+		public int GeneratedTextLength { get; set; } = 1;
+		public int GeneratedErrorCount { get; set; } = 0;
+		public bool GenerateTextData { get; set; } = true;
+		public bool GenerateTransmissionErrors { get; set; } = true;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void GenerateText()
+		{
+			var chars = Enumerable.Repeat(1, GeneratedTextLength).Select(i => (char)_random.Next('a', 'z' + 1));
+			TextData = string.Concat(chars);
+
+			ConvertTextToBits();
+		}
+
+		public void ConvertTextToBits()
+		{
+			var textBytes = Encoding.ASCII.GetBytes(TextData);
+			_originalBits = textBytes.SelectMany(b => b.ToBits()).ToArray();
+			TransmittedBits = (byte[])_originalBits.Clone();
+			ModifiedBits = new bool[TransmittedBits.Length];
+
+			DataModel = new BitData(new byte[8], TransmittedBits);
+		}
+
+		public void GenerateErrors()
+		{
+			TransmittedBits = (byte[])_originalBits.Clone();
+
+			var bitErrors = new bool[TransmittedBits.Length];
+			for (int i = 0; i < GeneratedErrorCount; i++)
+			{
+				int index;
+				do
+					index = _random.Next(TransmittedBits.Length);
+				while (bitErrors[index]);
+
+				bitErrors[index ] = true;
+				TransmittedBits[index] ^= 1;
+			}
+			ModifiedBits = bitErrors;
+		}
+	}
+}
---
+++ CorrectionCodes/UI/SuccinctGrid.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CorrectionCodes.UI
+{
+	public sealed class SuccinctGrid : Grid
+	{
+		public GridLengthCollection Rows
+		{
+			get { return null; }
+			set
+			{
+				var rows = value;
+				if (rows == null)
+					return;
+				RowDefinitions.Clear();
+				foreach (var length in rows.Values)
+				{
+					RowDefinitions.Add(new RowDefinition { Height = length });
+				}
+			}
+		}
+		
+		public GridLengthCollection Columns
+		{
+			get { return null; }
+			set
+			{
+				var columns = value;
+				if (columns == null)
+					return;
+				ColumnDefinitions.Clear();
+				foreach (var length in columns.Values)
+				{
+					ColumnDefinitions.Add(new ColumnDefinition { Width = length });
+				}
+			}
+		}
+	}
+
+	[TypeConverter(typeof(GridLengthCollectionConverter))]
+	public sealed class GridLengthCollection 
+	{	
+		private IEnumerable<GridLength> _values;
+
+		public GridLengthCollection(IEnumerable<GridLength> values)
+		{
+			_values = values;
+		}
+
+		public IEnumerable<GridLength> Values
+		{
+			get { return _values; }
+			set { _values = value; }
+		}
+	}
+
+	public sealed class GridLengthCollectionConverter: TypeConverter
+	{
+		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+		{
+			if (sourceType == typeof(string))
+				return true;
+			return base.CanConvertFrom(context, sourceType);
+		}
+ 
+		public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+		{
+			if (destinationType == typeof(string))
+				return true;
+			return base.CanConvertTo(context, destinationType);
+		}
+ 
+		public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+		{
+			if (value is string s)
+				return ParseString(s);
+			return base.ConvertFrom(context, culture, value);
+		}
+ 
+		public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+		{
+			if (destinationType == typeof(string) && value is GridLengthCollection collection)
+				return ToString(collection, culture);
+			return base.ConvertTo(context, culture, value, destinationType);
+		}
+ 
+		private string ToString(GridLengthCollection value, CultureInfo culture)
+		{
+			var converter = new GridLengthConverter();
+			return string.Join(",", value.Values.Select(v => converter.ConvertToString(v)));
+		}
+ 
+		private static GridLengthCollection ParseString(string s)
+		{
+			var converter = new GridLengthConverter();
+			var lengths   = s.Split(',').Select(p => (GridLength)converter.ConvertFromString(p.Trim()));
+			return new GridLengthCollection(lengths);
+		}
+	}
+}
