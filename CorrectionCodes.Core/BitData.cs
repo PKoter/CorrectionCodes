383ca814c545c4b05462b0a6a8c69761db20c3d7
Brat Wiekszy <bratwiekszy@gmail.com>
2019-05-07 11:08:51 +0200

    Added a bit of statistics

--- CorrectionCodes.Core/BitData.cs
+++ CorrectionCodes.Core/BitData.cs
@@ -1,20 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using JetBrains.Annotations;
+﻿using JetBrains.Annotations;
 
 namespace CorrectionCodes.Core
 {
 	public sealed class BitData
 	{
-		[NotNull] public byte[] CorrectionBits { get; set; }
+		[NotNull] public byte[] CorrectionBits  { get; set; }
 		[NotNull] public byte[] TransmittedBits { get; private set; }
 
-		public bool[] DetectedBitErrors { get; set; }
-		public byte[] FixedTransmittedBits { get; set; }
+		public bool[] DetectedBitErrors     { get; set; }
+		public byte[] FixedTransmittedBits  { get; set; }
+		public bool   IncorrectTransmission { get; set; }
+
+		public int DataBitCount => TransmittedBits.Length;
+		public int CorrectionBitCount => CorrectionBits.Length;
+		public int PayloadBitCount => CorrectionBitCount + DataBitCount;
 
 
-		public BitData(byte[] correctionBits, byte[] transmittedBits)
+		public BitData([NotNull] byte[] correctionBits, [NotNull] byte[] transmittedBits)
 		{
 			CorrectionBits = correctionBits;
 			TransmittedBits = transmittedBits;
