700caa64f1c662fd44be12e7df13c972ae50dd48
Brat Wiekszy <bratwiekszy@gmail.com>
2019-05-15 17:08:13 +0200

    Moved algo-specific code to own control

---
+++ CorrectionCodes/AlgorithmContextControl.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CorrectionCodes
+{
+	/// <summary>
+	/// Interaction logic for AlgorithmContextControl.xaml
+	/// </summary>
+	public partial class AlgorithmContextControl : UserControl
+	{
+		private MainController _controller;
+
+		public AlgorithmContextControl()
+		{
+			InitializeComponent();
+		
+		}
+
+		public MainController Controller
+		{
+			get => _controller;
+			set
+			{
+				_controller = value;
+				this.DataContext = value;
+			}
+		}
+
+		private void GenerateTextData(object sender, RoutedEventArgs e)
+		{
+			_controller.GenerateText();
+		}
+
+		private void OnMainGridMouseDown(object sender, MouseButtonEventArgs e)
+		{
+			Keyboard.ClearFocus();
+		}
+
+		private void OnTextInputLostFocus(object sender, RoutedEventArgs e)
+		{
+			_controller.ConvertTextToBits();
+		}
+
+		private void GenerateErrors(object sender, RoutedEventArgs e)
+		{
+			_controller.GenerateErrors();
+
+		}
+	}
+}
