b8164d992e7cd0e99948825913eb658cd83a5bab
Brat Wiekszy <bratwiekszy@gmail.com>
2019-05-15 14:25:49 +0200

    Added crc algorithms

---
+++ CorrectionCodes.Core/Crc32.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace CorrectionCodes.Core
+{
+	public sealed class Crc32 : IByteBasedCode
+	{
+		private static readonly uint[] _table;
+		
+		static Crc32()
+		{
+			const uint poly = 0x04C11DB7;
+			_table = new uint[256];
+			for (uint i = 0; i < _table.Length; i++)
+			{
+				uint temp = i;
+				for (int j = 8; j > 0; --j)
+				{
+					if ((temp & 1) == 1)
+					{
+						temp = ((temp >> 1) ^ poly);
+					}
+					else
+					{
+						temp >>= 1;
+					}
+				}
+				_table[i] = temp;
+			}
+		}
+
+		public static uint ComputeChecksum(byte[] bytes)
+		{
+			uint crc = 0;
+			for (int i = 0; i < bytes.Length; i++)
+			{
+				byte index = (byte)((crc & 0xff) ^ bytes[i]);
+				crc = ((crc >> 8) ^ _table[index]);
+			}
+			return crc ;
+		}
+
+		public byte[] ComputeCode(byte[] rawBytes)
+		{
+			var crc = ComputeChecksum(rawBytes);
+			return BitConverter.GetBytes(crc);
+		}
+
+		public void DetectBitErrors(BitData transmittedData)
+		{
+			var inputBytes = transmittedData.TramsmittedBytes;
+			var crcBytes   = transmittedData.CorrectionBytes;
+			var payload    = inputBytes.Concat(crcBytes).ToArray();
+			var crc        = ComputeChecksum(payload);
+
+			transmittedData.IncorrectTransmission = crc != 0;
+		}
+	}
+}
---
+++ Tests/CrcTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using CorrectionCodes.Core;
+using FluentAssertions;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Tests
+{
+	[TestFixture]
+	public class CrcTests
+	{
+		[TestCase("abcs")]
+		[TestCase("11aaaee09")]
+		[TestCase("x")]
+		public void Crc16_CrcWithPayloadComputesToZero(string input)
+		{
+			var bytes = input.ToBytes();
+			var crc = Crc16.ComputeChecksum(bytes);
+
+			var payload = bytes.Concat(BitConverter.GetBytes(crc)).ToArray();
+			var secondCrc = Crc16.ComputeChecksum(payload);
+
+			secondCrc.Should().Be(0);
+		}
+
+		[TestCase("abcs")]
+		[TestCase("11aaaee09")]
+		[TestCase("x")]
+		public void Crc16_CrcWithModifiedPayloadComputesToNotZero(string input)
+		{
+			var bytes = input.ToBytes();
+			var crc   = Crc16.ComputeChecksum(bytes);
+
+			bytes[0] ^= 0x81;
+			var payload   = bytes.Concat(BitConverter.GetBytes(crc)).ToArray();
+			var secondCrc = Crc16.ComputeChecksum(payload);
+
+			secondCrc.Should().NotBe(0);
+		}
+
+		[TestCase("abcs")]
+		[TestCase("11aaaee09")]
+		[TestCase("x")]
+		public void Crc32_CrcWithPayloadComputesToZero(string input)
+		{
+			var bytes = input.ToBytes();
+			var crc   = Crc32.ComputeChecksum(bytes);
+
+			var payload   = bytes.Concat(BitConverter.GetBytes(crc)).ToArray();
+			var secondCrc = Crc32.ComputeChecksum(payload);
+
+			secondCrc.Should().Be(0);
+		}
+
+		[TestCase("abcs")]
+		[TestCase("11aaaee09")]
+		[TestCase("x")]
+		public void Crc32_CrcWithModifiedPayloadComputesToNotZero(string input)
+		{
+			var bytes = input.ToBytes();
+			var crc   = Crc32.ComputeChecksum(bytes);
+			
+			bytes[0] ^= 0x14;
+			var payload   = bytes.Concat(BitConverter.GetBytes(crc)).ToArray();
+			var secondCrc = Crc32.ComputeChecksum(payload);
+
+			secondCrc.Should().NotBe(0);
+		}
+	}
+}
