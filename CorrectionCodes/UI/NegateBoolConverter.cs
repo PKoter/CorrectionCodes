45e6ceb7460ca2a742a0a5a7aea2a8b4565dc90d
Brat Wiekszy <bratwiekszy@gmail.com>
2019-05-15 17:07:31 +0200

    Added few converters

---
+++ CorrectionCodes/UI/NegateBoolConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CorrectionCodes.UI
+{
+	public sealed class NegateBoolConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value is bool b ? !b : value;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is bool b)
+				return !b;
+
+			return value;
+		}
+	}
+}
